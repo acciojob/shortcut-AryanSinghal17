ecf6ae37c666b1ce01b00ff5928afc3529f7d91e
Aryan Singhal <115396133+AryanSinghal17@users.noreply.github.com>
2026-07-23 21:39:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function shortcut(s1, s2) {
   // your code here
 if (s1.length === 0 || s2.length === 0) return "";
 let str = s1[0] + s2[0];
-console.log(str);
+return s1[0] + s2[0];
 }
 
 // Do not change the code below.
